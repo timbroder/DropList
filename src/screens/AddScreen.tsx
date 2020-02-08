c33ffb1e6f4274860368235516fc7bec0518fb19
Tim Broder <timothy.broder@gmail.com>
2020-02-08 17:41:09 -0500

    add string util

--- src/screens/AddScreen.tsx
+++ src/screens/AddScreen.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import {capitalize} from "../utils/string";
 
 interface AddScreenProps {
     what: string
 }
 
 const AddScreen = (props: AddScreenProps) => {
-    return <Text style={styles.text}>{ props.what }</Text>;
+    return <Text style={styles.text}>{ capitalize(props.what) }</Text>;
 };
 
 const styles = StyleSheet.create({
