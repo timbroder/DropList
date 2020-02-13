96f7e0ab0a9951647f780d7db95e9aec997c2518
Tim Broder <timothy.broder@gmail.com>
2020-02-12 21:01:46 -0500

    persist state

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {AsyncStorage} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import hardSet from "redux-persist/es/stateReconciler/hardSet";
+
 import ListsScreen from "./src/screens/ListsScreen";
 import AddListScreen from "./src/screens/AddListScreen";
 import reducer from "./src/redux/reducers";
@@ -10,19 +15,29 @@ import ListScreen from "./src/screens/ListScreen";
 import AddTodoScreen from "./src/screens/AddTodoScreen";
 
 const Stack = createStackNavigator();
-const store = createStore(reducer);
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    stateReconciler: hardSet
+}
+const persistedReducer = persistReducer(persistConfig, reducer)
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 function App() {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Lists" component={ListsScreen} />
-                    <Stack.Screen name="AddList" component={AddListScreen} />
-                    <Stack.Screen name="List" component={ListScreen} />
-                    <Stack.Screen name="AddTodo" component={AddTodoScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
+            <PersistGate loading={null} persistor={persistor}>
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen name="Lists" component={ListsScreen} />
+                        <Stack.Screen name="AddList" component={AddListScreen} />
+                        <Stack.Screen name="List" component={ListScreen} />
+                        <Stack.Screen name="AddTodo" component={AddTodoScreen} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </PersistGate>
         </Provider>
     );
 }
